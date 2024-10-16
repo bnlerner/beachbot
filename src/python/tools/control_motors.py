1d1105af5ea427db4a048d092d6229d96e03cf1a
Brian Lerner <bnlerner@gmail.com>
2024-10-16 11:56:37 -0400

    fixes type issues

--- src/python/tools/control_motors.py
+++ src/python/tools/control_motors.py
@@ -10,36 +10,32 @@ See https://docs.odriverobotics.com/v/latest/manual/can-protocol.html for protoc
 documentation.
 """
 import asyncio
-import pydantic
 import can
 import struct
+from config import motor_config
 
-
-class Motor(pydantic.BaseModel):
-    node_id: int
-    location: str
 
 _HEARTBEAT_CMD_ID = 0x01
-_ALL_MOTORS = [Motor(node_id=id, location="blah") for id in range(4)]
+_ALL_MOTORS = [motor_config.Motor(node_id=id, location="blah") for id in range(4)]
 # Get_Encoder_Estimates
 _ENCODER_ESTIMATES_ID = 0x09
 
 
 
-async def _control_motor(bus: can.interface.Bus, node_id:int) -> None:
+async def _control_motor(bus: can.interface.BusABC, node_id:int) -> None:
     """Node ID must match `<odrv>.axis0.config.can.node_id`. The default is 0."""
     # Put axis into closed loop control state
     _set_control_loop_state(bus, node_id)
     _set_velocity(bus, node_id, 1.0)
 
 
-def _flush_bus(bus: can.interface.Bus) -> None:
+def _flush_bus(bus: can.interface.BusABC) -> None:
     # Flush CAN RX buffer so there are no more old pending messages
     while not (bus.recv(timeout=0) is None):
         pass
 
 
-def _set_control_loop_state(bus: can.interface.Bus, node_id:int) -> None:
+def _set_control_loop_state(bus: can.interface.BusABC, node_id:int) -> None:
     bus.send(can.Message(
         arbitration_id=(node_id << 5 | 0x07), # 0x07: Set_Axis_State
         data=struct.pack('<I', 8), # 8: AxisState.CLOSED_LOOP_CONTROL
@@ -54,7 +50,7 @@ def _set_control_loop_state(bus: can.interface.Bus, node_id:int) -> None:
                 break
 
 
-def _set_velocity(bus: can.interface.Bus, node_id:int, velocity: float) -> None:
+def _set_velocity(bus: can.interface.BusABC, node_id:int, velocity: float) -> None:
     """Sets velocity in turns/s"""
     bus.send(can.Message(
         arbitration_id=(node_id << 5 | 0x0d), # 0x0d: Set_Input_Vel
@@ -62,26 +58,26 @@ def _set_velocity(bus: can.interface.Bus, node_id:int, velocity: float) -> None:
         is_extended_id=False
     ))
 
-def _print_heartbeat(bus: can.interface.Bus) -> None:
+def _print_heartbeat(bus: can.interface.BusABC) -> None:
     for msg in bus:
         for node_id in range(4):
             if msg.arbitration_id == (node_id << 5 | _HEARTBEAT_CMD_ID):
                 error, state, result, traj_done = struct.unpack('<IBBB', bytes(msg.data[:7]))
                 print(f"{node_id=}, {error=}, {state=}, {result=}, {traj_done=}")
 
-def _print_encoder_feedback(bus: can.interface.Bus) -> None:
+def _print_encoder_feedback(bus: can.interface.BusABC) -> None:
     for msg in bus:
         for node_id in range(4):
             if msg.arbitration_id == (node_id << 5 | _ENCODER_ESTIMATES_ID):
                 pos, vel = struct.unpack('<ff', bytes(msg.data))
                 print(f"{node_id=} pos: {pos:.3f} [turns], vel: {vel:.3f} [turns/s]")
 
-def _stop_all_motors(bus: can.interface.Bus) -> None:
+def _stop_all_motors(bus: can.interface.BusABC) -> None:
     for motor in _ALL_MOTORS:
         _set_velocity(bus, motor.node_id, 0.0)
 
 
-async def main(bus: can.interface.Bus) -> None:
+async def main(bus: can.interface.BusABC) -> None:
     tasks = [asyncio.create_task(_control_motor(bus, motor.node_id)) for motor in _ALL_MOTORS]
     await asyncio.gather(*tasks)
     # Print encoder feedback
