35fddd6351e4a99534a330fbc2a6e817d93adb30
Brian Lerner <bnlerner@gmail.com>
2024-10-17 12:43:37 -0400

    adds motor configs, can tools including messages and connection wrapper + enums.

--- src/python/tools/control_motors.py
+++ src/python/tools/control_motors.py
@@ -10,88 +10,80 @@ See https://docs.odriverobotics.com/v/latest/manual/can-protocol.html for protoc
 documentation.
 """
 import asyncio
-import can
-import struct
 from config import motor_config
+from odrive import enums as odrive_enums  # type: ignore[import-untyped]
+from drivers.can import connection, enums, messages
+
+_ALL_MOTORS = [
+    motor_config.MotorConfig(node_id=0, location=motor_config.MotorLocation.FRONT_LEFT),
+    motor_config.MotorConfig(node_id=1, location=motor_config.MotorLocation.FRONT_RIGHT),
+    motor_config.MotorConfig(node_id=2, location=motor_config.MotorLocation.REAR_LEFT),
+    motor_config.MotorConfig(node_id=3, location=motor_config.MotorLocation.REAR_RIGHT),
+]
 
 
-_HEARTBEAT_CMD_ID = 0x01
-_ALL_MOTORS = [motor_config.Motor(node_id=id, location="blah") for id in range(4)]
-# Get_Encoder_Estimates
-_ENCODER_ESTIMATES_ID = 0x09
-
-
-
-async def _control_motor(bus: can.interface.BusABC, node_id:int) -> None:
+async def _control_motor(bus: connection.CANSimple, node_id:int) -> None:
     """Node ID must match `<odrv>.axis0.config.can.node_id`. The default is 0."""
     # Put axis into closed loop control state
-    _set_control_loop_state(bus, node_id)
-    _set_velocity(bus, node_id, 1.0)
+    await _set_control_loop_state(bus, node_id)
+    await _set_velocity(bus, node_id, 1.0)
 
 
-def _flush_bus(bus: can.interface.BusABC) -> None:
-    # Flush CAN RX buffer so there are no more old pending messages
-    while not (bus.recv(timeout=0) is None):
-        pass
-
-
-def _set_control_loop_state(bus: can.interface.BusABC, node_id:int) -> None:
-    bus.send(can.Message(
-        arbitration_id=(node_id << 5 | 0x07), # 0x07: Set_Axis_State
-        data=struct.pack('<I', 8), # 8: AxisState.CLOSED_LOOP_CONTROL
-        is_extended_id=False
-    ))
-
+async def _set_control_loop_state(bus: connection.CANSimple, node_id:int) -> None:
+    axis_state = odrive_enums.AxisState.CLOSED_LOOP_CONTROL.value
+    axis_msg = messages.SetAxisStateMessage(node_id, axis_state=axis_state)
+    await bus.send(axis_msg)
+    listener = connection.CANSimpleListener[messages.HeartbeatMessage]()
     # Wait for axis to enter closed loop control by scanning heartbeat messages
-    for msg in bus:
-        if msg.arbitration_id == (node_id << 5 | 0x01): # 0x01: Heartbeat
-            error, state, result, traj_done = struct.unpack('<IBBB', bytes(msg.data[:7]))
-            if state == 8: # 8: AxisState.CLOSED_LOOP_CONTROL
-                break
+    while True:
+        msg = await listener.get_message()
+        if msg.node_id == node_id and msg.axis_state == axis_state:
+            break
 
 
-def _set_velocity(bus: can.interface.BusABC, node_id:int, velocity: float) -> None:
+async def _set_velocity(bus: connection.CANSimple, node_id:int, velocity: float) -> None:
     """Sets velocity in turns/s"""
-    bus.send(can.Message(
-        arbitration_id=(node_id << 5 | 0x0d), # 0x0d: Set_Input_Vel
-        data=struct.pack('<ff', velocity, 0.0), # 1.0: velocity, 0.0: torque feedforward
-        is_extended_id=False
-    ))
+    vel_msg = messages.SetVelocityMessage(node_id, velocity=velocity)
+    await bus.send(vel_msg)
 
-def _print_heartbeat(bus: can.interface.BusABC) -> None:
-    for msg in bus:
-        for node_id in range(4):
-            if msg.arbitration_id == (node_id << 5 | _HEARTBEAT_CMD_ID):
-                error, state, result, traj_done = struct.unpack('<IBBB', bytes(msg.data[:7]))
-                print(f"{node_id=}, {error=}, {state=}, {result=}, {traj_done=}")
 
-def _print_encoder_feedback(bus: can.interface.BusABC) -> None:
-    for msg in bus:
-        for node_id in range(4):
-            if msg.arbitration_id == (node_id << 5 | _ENCODER_ESTIMATES_ID):
-                pos, vel = struct.unpack('<ff', bytes(msg.data))
-                print(f"{node_id=} pos: {pos:.3f} [turns], vel: {vel:.3f} [turns/s]")
+async def _print_heartbeat(bus: connection.CANSimple) -> None:
+    bus.register_callbacks((messages.HeartbeatMessage, _print_heartbeat_msg))
+    await bus.listen()
 
-def _stop_all_motors(bus: can.interface.BusABC) -> None:
+
+def _print_heartbeat_msg(msg: messages.HeartbeatMessage) -> None:
+    print(f"{msg.node_id=}, {msg.axis_error=}, {msg.procedure_result=}, {msg.axis_state=}, {msg.trajectory_done_flag=}")
+
+
+async def _print_encoder_feedback(bus: connection.CANSimple) -> None:
+    bus.register_callbacks((messages.EncoderEstimatesMessage, _print_encoder_data))
+    await bus.listen()
+
+
+def _print_encoder_data(msg: messages.EncoderEstimatesMessage) -> None:
+    print(f"{msg.arbitration_id=}, pos: {msg.pos_estimate:.3f} [turns], vel: {msg.vel_estimate:.3f} [turns/s]")
+
+
+async def _stop_all_motors(bus: connection.CANSimple) -> None:
     for motor in _ALL_MOTORS:
-        _set_velocity(bus, motor.node_id, 0.0)
+        await _set_velocity(bus, motor.node_id, 0.0)
 
 
-async def main(bus: can.interface.BusABC) -> None:
-    tasks = [asyncio.create_task(_control_motor(bus, motor.node_id)) for motor in _ALL_MOTORS]
-    await asyncio.gather(*tasks)
-    # Print encoder feedback
-    _print_encoder_feedback(bus)
-
-if __name__ == "__main__":
-    bus = can.interface.Bus("can0", bustype="socketcan")
-    _flush_bus(bus)
+async def main(bus: connection.CANSimple) -> None:
     try:
-        print("Starting motor control")
-        asyncio.run(main(bus))
+        for motor in _ALL_MOTORS:
+            await _control_motor(bus, motor.node_id)
+        # Print encoder feedback
+        await _print_encoder_feedback(bus)
     except KeyboardInterrupt:
         pass
+    finally:
+        await _stop_all_motors(bus)
 
-    _stop_all_motors(bus)
+if __name__ == "__main__":
+    bus = connection.CANSimple(enums.CANInterface.ODRIVE, enums.BusType.SOCKET_CAN)
+    print("Starting motor control")
+    asyncio.run(main(bus))
     bus.shutdown()
     print("Shutting down")
