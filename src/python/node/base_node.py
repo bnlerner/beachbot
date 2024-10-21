2dc066c05192dc5c29eaa222a61495148263603d
Brian Lerner <bnlerner@gmail.com>
2024-10-21 13:07:33 -0700

    updates from testing

--- src/python/node/base_node.py
+++ src/python/node/base_node.py
@@ -6,30 +6,33 @@ import functools
 class BaseNode:
     """A node which defines common functionality used in other nodes"""
 
-    def __init__(self):
-
+    def __init__(self) -> None:
         # Maintains a reference to the task running in the node so its cancellable.
         self._node_task: Optional[asyncio.Task] = None
         self._background_tasks: List[asyncio.Task] = []
         self._task_functions: List[Callable] = []
 
-    def add_tasks(self, *tasks: Callable) -> None:
-        self._task_functions.extend(tasks)
+    def add_tasks(self, *functions: Callable) -> None:
+        self._task_functions.extend(functions)
 
     def start(self) -> None:
         asyncio.run(self._async_run())
 
     async def _async_run(self) -> None:
         self._node_task = asyncio.create_task(self._main())
+        await self._node_task
 
     async def _main(self) -> None:
+        exception: Optional[Exception] = None
         try:
             self._create_tasks()
             await asyncio.gather(*self._background_tasks)
+        except Exception as err:
+            exception = err
         finally:
             self.shutdown()
             await self.shutdown_hook()
-
+            await self._raise_exceptions(exception)
 
     def shutdown(self) -> None:
         for task in self._background_tasks:
@@ -37,7 +40,7 @@ class BaseNode:
 
     def _create_tasks(self) -> None:
         for function in self._task_functions:
-            if asyncio.iscoroutinefunction(functools._unwrap_partial(function)):
+            if asyncio.iscoroutinefunction(functools._unwrap_partial(function)):  # type: ignore[attr-defined]
                 coroutine = function()
             else:
                 coroutine = asyncio.to_thread(function)
@@ -47,3 +50,15 @@ class BaseNode:
     async def shutdown_hook(self) -> None:
         """Runs prior to the node shutting down. Overridden by implementation of the node."""
         ...
+
+    async def _raise_exceptions(self, exception: Optional[BaseException]) -> None:
+        results = await asyncio.gather(*self._background_tasks, return_exceptions=True)
+        gather_exceptions = [r for r in results if isinstance(r, BaseException)]
+        if exception:
+            gather_exceptions.append(exception)
+
+        for exception in gather_exceptions:
+            if isinstance(exception, asyncio.CancelledError) or isinstance(exception, KeyboardInterrupt):
+                pass
+            else:
+                raise exception
--- src/python/node/motor_control_node.py
+++ src/python/node/motor_control_node.py
@@ -1,5 +1,10 @@
 import asyncio
 from odrive import enums as odrive_enums  # type: ignore[import-untyped]
+import sys
+import os
+
+# Get the path to the root of the project
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from config import motor_config
 from drivers.can import connection, enums, messages
@@ -13,18 +18,18 @@ _INITIAL_VELOCITY = 1.0
 class MotorControlNode(base_node.BaseNode):
     """A Node to control the ODrive motor controllers via a CAN bus interface."""
 
-    def __init__(self):
+    def __init__(self) -> None:
         super().__init__()
         self._can_bus = connection.CANSimple(enums.CANInterface.ODRIVE, enums.BusType.SOCKET_CAN)
         self._motor_configs = session.get_robot_motor_configs("beachbot-1")
 
         self._can_bus.register_callbacks(
-            (messages.EncoderEstimatesMessage, print),
-            (messages.HeartbeatMessage, print),
+            (messages.EncoderEstimatesMessage, _async_print_msg),
+            (messages.HeartbeatMessage, _async_print_msg),
         )
         self.add_tasks(self._startup_motors, self._can_bus.listen)
 
-    async def shutdown_hook(self):
+    async def shutdown_hook(self) -> None:
         # Sends a zero velocity to stop the motors.
         for motor in self._motor_configs:
             await self._send_velocity_cmd(motor, 0.0)
@@ -37,14 +42,19 @@ class MotorControlNode(base_node.BaseNode):
                 motor.node_id,
                 axis_state=odrive_enums.AxisState.CLOSED_LOOP_CONTROL.value
             )
-            await asyncio.sleep(0.01)
             await self._can_bus.send(axis_msg)
+            # Wait for the motor to set and respond
+            await asyncio.sleep(0.1)
             await self._send_velocity_cmd(motor, _INITIAL_VELOCITY)
 
     async def _send_velocity_cmd(self, motor: motor_config.MotorConfig, velocity: float) -> None:
         signed_velocity = motor.direction * velocity
         vel_msg = messages.SetVelocityMessage(motor.node_id, velocity=signed_velocity)
         await self._can_bus.send(vel_msg)
+
+
+async def _async_print_msg(msg: messages.OdriveCanMessage) -> None:
+    print(msg)
 
 if __name__ == "__main__":
     node = MotorControlNode()
