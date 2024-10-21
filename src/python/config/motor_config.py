dec163fbabc1c81a044cfd8807185718bcd17c4d
Brian Lerner <bnlerner@gmail.com>
2024-10-21 12:02:59 -0700

    Brian/add messages connection (#4)
    * adds motor configs, can tools including messages and connection wrapper + enums.
    
    * mods from testing
    
    * type issues with connection
    
    * adds way to load motor configs and implements session objects for loading them. adds a way to get the system level project directory
    
    * adds mypy ini file
    
    * small zhuzhes
    
    * adds repr vs print function
    
    * moves to 3.12

--- src/python/config/motor_config.py
+++ src/python/config/motor_config.py
@@ -1,7 +1,9 @@
+from __future__ import annotations
 from typing import Literal
 import pydantic
 import enum
-
+import pathlib
+import json
 
 class MotorLocation(enum.Enum):
     FRONT_LEFT = "FRONT_LEFT"
@@ -16,7 +18,24 @@ class MotorConfig(pydantic.BaseModel):
 
     @property
     def direction(self) -> Literal[1, -1]:
+        """The velocity direction relative to the mount point on the chassis.
+        Motors on the left chassis need to turn in reverse to all respond in the
+        same way to a positive body velocity.
+        """
         if self.location in (MotorLocation.FRONT_LEFT, MotorLocation.REAR_LEFT):
             return -1
         else:
             return 1
+
+    @classmethod
+    def from_json(cls, file_path: pathlib.Path) -> MotorConfig:
+        if not file_path.exists():
+            raise ValueError(f"File path does not exist. {file_path}")
+
+        with open(file_path, "r") as f:
+            motor_config_dict = json.load(f)
+
+        location = MotorLocation(file_path.stem.upper())
+        node_id = motor_config_dict["axis0.config.can.node_id"]
+
+        return MotorConfig(node_id=node_id, location=location)
