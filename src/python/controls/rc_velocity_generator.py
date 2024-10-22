628f81dc2c6ab02a35d0a8cc182ff069e8ab5dc8
Brian Lerner <bnlerner@gmail.com>
2024-10-22 10:04:56 -0700

    adds rc motor control; (#5)
    * adds rc motor control;
    
    * adds a rc tool
    
    * updates from testing
    
    * adds a docstring
    
    * adds pypi packages for input

---
+++ src/python/controls/rc_velocity_generator.py
@@ -0,0 +1,72 @@
+from typing import DefaultDict, Dict, List
+from pynput import keyboard
+import collections
+
+from config import motor_config
+
+
+class RCVelocityGenerator:
+    """Inputs a motor velocity and the motor configs and will output the motor velocities to respond to key presses."""
+
+    def __init__(self, velocity: float, motor_configs: List[motor_config.MotorConfig]) -> None:
+        self._velocity_default = velocity
+        self._motor_configs = motor_configs
+        self._pressed_keys: DefaultDict[keyboard.Key, bool] = collections.defaultdict(lambda: False)
+
+    def update(self, key: keyboard.Key, *, pressed: bool) -> None:
+        self._pressed_keys[key] = pressed
+
+    def velocities(self) -> Dict[motor_config.MotorConfig, float]:
+        return {
+            motor: self._calc_motor_velocity(motor) for motor in self._motor_configs
+        }
+
+    def _calc_motor_velocity(self, motor: motor_config.MotorConfig) -> float:
+        linear_vel = self._linear_velocity()
+        angular_vel = self._angular_velocity()
+
+        if motor.location in (motor_config.MotorLocation.FRONT_LEFT, motor_config.MotorLocation.REAR_LEFT):
+            return (linear_vel - angular_vel) * motor.direction
+        elif motor.location in (motor_config.MotorLocation.FRONT_RIGHT, motor_config.MotorLocation.REAR_RIGHT):
+            return (linear_vel + angular_vel) * motor.direction
+        else:
+            raise ValueError(f"Unknown motor config location {motor=}")
+
+    def _angular_velocity(self) -> float:
+        """Angular velocity assuming that the velocity rotates about the robots body axis which would be with 
+        the x-axis pointed forward and the z-axis pointed skyward.
+        """
+        vel = 0.0
+        if self._right_key_pressed:
+            vel -= self._velocity_default
+        # Left turn is a positive angular velocity.
+        if self._left_key_pressed:
+            vel += self._velocity_default
+        
+        return vel
+
+    def _linear_velocity(self) -> float:
+        vel = 0.0
+        if self._up_key_pressed:
+            vel += self._velocity_default
+        if self._down_key_pressed:
+            vel -= self._velocity_default
+        
+        return vel
+
+
+    @property
+    def _up_key_pressed(self) -> bool:
+        return self._pressed_keys[keyboard.Key.up]
+    
+    @property
+    def _down_key_pressed(self) -> bool:
+        return self._pressed_keys[keyboard.Key.down]
+        
+    @property
+    def _left_key_pressed(self) -> bool:
+        return self._pressed_keys[keyboard.Key.left]
+
+    @property
+    def _right_key_pressed(self) -> bool:
+        return self._pressed_keys[keyboard.Key.right]
---
+++ src/python/tools/rc_robot.py
@@ -0,0 +1,60 @@
+"""
+A simple example to print RC commands.
+"""
+import asyncio
+import sys
+import os
+from pynput import keyboard
+from typing import Optional, Union
+
+# Get the path to the root of the project
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from node import base_node
+
+class RCRobotNode(base_node.BaseNode):
+
+    def __init__(self) -> None:
+        super().__init__()
+        self._rc_listener = keyboard.Listener(on_press=self._on_press, on_release=self._on_release)
+        self._rc_listener.start()
+
+        self.add_tasks(self._loop_and_no_nothing)
+
+    def _on_press(self, key: Optional[Union[keyboard.Key, keyboard.KeyCode]]) -> None:
+        try:
+            if key == keyboard.Key.up:
+                print('Up arrow key pressed')
+            elif key == keyboard.Key.down:
+                print('Down arrow key pressed')
+            elif key == keyboard.Key.left:
+                print('Left arrow key pressed')
+            elif key == keyboard.Key.right:
+                print('Right arrow key pressed')
+            elif key == keyboard.Key.esc:
+                exit(0)
+        except AttributeError:
+            print('special key {0} pressed'.format(key))
+
+
+    def _on_release(self, key: Optional[Union[keyboard.Key, keyboard.KeyCode]]) -> None:
+        if key == keyboard.Key.up:
+            print('Up arrow key released')
+        elif key == keyboard.Key.down:
+            print('Down arrow key released')
+        elif key == keyboard.Key.left:
+            print('Left arrow key released')
+        elif key == keyboard.Key.right:
+            print('Right arrow key released')
+
+    async def _loop_and_no_nothing(self) -> None:
+        while True:
+            await asyncio.sleep(10)
+
+    async def shutdown_hook(self) -> None:
+        self._rc_listener.stop()
+
+
+if __name__ == "__main__":
+    node = RCRobotNode()
+    node.start()
